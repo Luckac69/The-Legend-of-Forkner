c817877613b32082a044700e45fb594ef41c34f4
Luckac69 <145867659+Luckac69@users.noreply.github.com>
2024-02-07 22:24:51 -0500

    BowV.1
    Bow now exsists

--- bow.gd
+++ bow.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.hide() # Replace with function body.
@@ -8,8 +9,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rotate(-10*delta)
+	self.rotation = get_angle_to(get_global_mouse_position())
 
 
-func _on_timer_timeout():
-	self.hide()
+
--- main_character.gd
+++ main_character.gd
@@ -4,23 +4,41 @@ extends CharacterBody2D
 const SPEED = 300.0
 const STOP = .1
 
-@onready var wepon = $Wepon
+var isSword = false
+var isBow = false
 
+@onready var sword = $Sword
+@onready var bow = $Bow
+
+const bulletPath = preload("res://bullet.tscn")
 
 func _physics_process(delta):
-
 	_movement()
 	
-	if(Input.is_action_just_pressed("Click")):
-		_attack(delta)
+	if(Input.is_action_just_pressed("sword")):
+		isSword = true
+		
+	if(Input.is_action_just_pressed("bow")):
+		isBow = true
+	
+	if(Input.is_action_just_pressed("Click") && isSword):
+		_sword(delta)
+	
+	if(Input.is_action_just_pressed("Click") && isBow):
+		_bow(delta)
+
+func _bow(delta):
+	bow.show()
+	var bullet = bulletPath.instance()
+	get_parent().add_child(bullet)
+	
 	
 
-func _attack(delta):
-	wepon.show()
-	wepon.rotation = get_angle_to(get_global_mouse_position())+90
-	$Wepon/Timer.start()
-	wepon.rotate(5*delta)
-	
+func _sword(delta):
+	sword.show()
+	sword.rotation = get_angle_to(get_global_mouse_position())+90
+	$Sword/Timer.start()
+	sword.rotate(5*delta)
 
 
 
