7c4887ac2c61104d44c9a1155a19bdca08506271
Luckac69 <145867659+Luckac69@users.noreply.github.com>
2024-02-10 18:52:40 -0500

    Start of animations
    not done yet

--- Code/MainCharacterScripts/main_character.gd
+++ Code/MainCharacterScripts/main_character.gd
@@ -9,11 +9,19 @@ var isBow = false
 
 @onready var sword = $Sword
 @onready var bow = $Bow
+@onready var animation = $AnimationPlayer
+@onready var animation_tree = $AnimationTree
+
 
 const bulletPath = preload("res://Code/MainCharacterScripts/bullet.tscn")
 
+func _ready():
+	animation_tree.active = true
+
 func _physics_process(delta):
 	_movement()
+	
+	update_animation_parameters()
 	
 	if(Input.is_action_just_pressed("sword")):
 		if(isSword == false): 
@@ -52,13 +60,29 @@ func _sword(delta):
 	$Sword/Timer.start()
 	sword.rotate(5*delta)
 
-
+func update_animation_parameters():
+	if(velocity == Vector2.ZERO):
+		animation_tree["parameters/conditions/is_moving"] = false
+		animation_tree["parameters/conditions/idle"] = true
+	else:
+		animation_tree["parameters/conditions/is_moving"] = true
+		animation_tree["parameters/conditions/idle"] = false
+	
+	if(Input.is_action_just_pressed("Click")):
+		animation_tree["parameters/conditions/swing"] = true
+	else:
+		animation_tree["parameters/conditions/swing"] = false
+	
 
 func _movement():
 	# Horizantal
 	var xdirection = Input.get_axis("Left", "Right")
 	if xdirection:
 		velocity.x = xdirection * SPEED
+		if(xdirection>0):
+			$Sprite2D.flip_h = false
+		if(xdirection<0):
+			$Sprite2D.flip_h = true
 	else:
 		velocity.x = lerp(velocity.x, 0.0, STOP)
 	
@@ -69,5 +93,5 @@ func _movement():
 	else:
 		velocity.y = lerp(velocity.y, 0.0, STOP)
 	
-	print(position)
+	print(velocity)
 	move_and_slide()
